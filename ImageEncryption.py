c3c8d01e2a2f76c7334932045f84c81a5dc66ded
DevranCanikli <devrancanikli@outlook.com>
2019-10-09 19:13:52 +0300

    Encrypted image

--- ImageEncryption.py
+++ ImageEncryption.py
@@ -32,3 +32,45 @@ for i in range(0, 16):
         row = int(value[0], 16)
         column = int(value[1], 16)
         inverseSBox[row, column] = np.base_repr(i, 16) + np.base_repr(j, 16)
+
+im = Image.open('watch.png')
+rgb_im = im.convert('RGB')
+pixels = im.load()
+print(im.size)
+
+for i in range(im.size[0]):
+    for j in range(im.size[1]):
+        r, g, b = rgb_im.getpixel((i, j))
+
+        if r <= 15:
+            hexR = "0" + np.base_repr(r, 16)
+        else:
+            hexR = np.base_repr(r, 16)
+
+        if g <= 15:
+            hexG = "0" + np.base_repr(g, 16)
+        else:
+            hexG = np.base_repr(g, 16)
+
+        if b <= 15:
+            hexB = "0" + np.base_repr(b, 16)
+        else:
+            hexB = np.base_repr(b, 16)
+
+        rowR = int(hexR[0], 16)
+        columnR = int(hexR[1], 16)
+
+        rowG = int(hexG[0], 16)
+        columnG = int(hexG[1], 16)
+
+        rowB = int(hexB[0], 16)
+        columnB = int(hexB[1], 16)
+
+        newR = int(sBox[random.randint(0, 15), random.randint(0, 15)], 16) ^ int(sBox[rowR, columnR], 16)
+        newG = int(sBox[random.randint(0, 15), random.randint(0, 15)], 16) ^ int(sBox[rowG, columnG], 16)
+        newB = int(sBox[random.randint(0, 15), random.randint(0, 15)], 16) ^ int(sBox[rowB, columnB], 16)
+
+        pixels[i, j] = newR, newG, newB
+
+#im.save("encryptedImage.jpg")
+im.show()
