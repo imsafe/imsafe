ed13adc788bc5be324a034b98db4583d493995ea
alpsakaci <erenalpsakaci@gmail.com>
2019-10-13 20:38:22 +0300

    histogram test

--- Utility.py
+++ Utility.py
@@ -1,8 +1,17 @@
 import numpy as np
-
+import cv2 as cv
+from matplotlib import pyplot as plt
 
 def convertDecToHex(decimalNumber):
     if decimalNumber <= 15:
         return "0" + np.base_repr(decimalNumber, 16)
     else:
         return np.base_repr(decimalNumber, 16)
+
+
+def imageHistogram():
+    img = cv.imread('cat.png', 0)
+    hist = cv.calcHist([img], [0], None, [256], [0, 256])
+    hist, bins = np.histogram(img.ravel(), 256, [0, 256])
+    plt.hist(img.ravel(), 256, [0, 256])
+    plt.show()
